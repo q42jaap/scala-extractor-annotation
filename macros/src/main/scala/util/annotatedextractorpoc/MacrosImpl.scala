99336deae73319fc9c83b102e385eb449804e180
Jaap Taal <jaap@q42.nl>
2014-02-09 13:53:36 +0100

    The main question to be answered, when to raise a compiler error for missing implicit???

--- macros/src/main/scala/util/annotatedextractorpoc/MacrosImpl.scala
+++ macros/src/main/scala/util/annotatedextractorpoc/MacrosImpl.scala
@@ -23,205 +23,172 @@ object MacrosImpl {
       }
     }
   }
-}
-
-private abstract class Helper[C <: Context, TSource: C#WeakTypeTag, T: C#WeakTypeTag, TAnn <: ExtractAnnotation : C#WeakTypeTag](val c: C) {
-
-  import c.universe._
-
-  val extractorType = c.weakTypeOf[Extractor[_, _, _]]
-
-  val annType = c.weakTypeOf[TAnn]
-  val sourceType = c.weakTypeOf[TSource]
-  val tType = c.weakTypeOf[T]
-
-  def echo(msg: String) {
-    c.echo(c.enclosingPosition, msg)
-  }
-
-  def abort(msg: String): Nothing = {
-    c.abort(c.enclosingPosition, msg)
-  }
-
-  val sourceTypeName = sourceType.typeSymbol.name
-
-  val sourceTypeNameExpr = c.Expr[String](Literal(Constant(sourceTypeName.toString)))
-
-  val tTypeName = tType.typeSymbol.name
-  val tTypeNameExpr = c.Expr[String](Literal(Constant(tTypeName.toString)))
-
-  val tCompanion = tType.typeSymbol.companionSymbol
-
-  def hasTAnn(param: Symbol): Boolean = {
-    param.annotations.exists(_.tpe =:= annType)
-  }
-  val sourceParams = caseClassParams(sourceType)
-
-  val annotatedParams: List[Symbol] = sourceParams.filter(hasTAnn)
-
-  def isCaseClass(param: Symbol): Boolean = {
-    param.typeSignature <:< c.weakTypeOf[Product]
-  }
-
-  def isIterableParamWithCaseClass(param: Symbol): Boolean = {
-    param.typeSignature <:< iterableType
-    //TODO check type argument isCaseClass
-  }
-
-  def isOptionParamWithCaseClass(param: Symbol): Boolean = {
-    param.typeSignature <:< optionType
-  }
-
-  val otherParams: List[Symbol] = {
-    sourceParams.filterNot(hasTAnn)
-  }
-
-  def getImplicitExtractorType(paramSourceType: c.Type): c.Type = {
-    appliedType(extractorType, List(paramSourceType, tType, annType))
-  }
-  
-//  val deepIterableParams = otherParams.filter { p =>
-//    p.typeSignature <:< iterableType
-//  }
-//
-//  val deepOptionParams = otherParams.filter { p =>
-//    p.typeSignature <:< optionType
-//  }
-//
-//  val deepCaseClassParams = otherParams.filter(isCaseClass)
 
 
-  val listType = c.weakTypeOf[List[_]]
-  val listOfTType = appliedType(listType, List(tType))
-  val listCompanionSymbol = listType.typeSymbol.companionSymbol
+  private abstract class Helper[C <: Context, TSource: C#WeakTypeTag, T: C#WeakTypeTag, TAnn <: ExtractAnnotation : C#WeakTypeTag](val c: C) {
 
-  val iterableType = c.weakTypeOf[Iterable[_]]
-  val iterableOfTType = appliedType(iterableType, List(tType))
+    import c.universe._
 
-  val optionType = c.weakTypeOf[Option[_]]
-  val optionOfTType = appliedType(optionType, List(tType))
+    val extractorType = c.weakTypeOf[Extractor[_, _, _]]
 
+    val annType = c.weakTypeOf[TAnn]
+    val sourceType = c.weakTypeOf[TSource]
+    val tType = c.weakTypeOf[T]
 
-  //val reader = c.inferImplicitValue(appliedType(readerType, List(A)))
-  //if(! reader.isEmpty)
-
-  def paramVal(obj: Tree, param: Symbol): Tree =
-    Select(obj, param.name)
-
-  def mkListResult(obj: Tree): Tree = {
-
-    // transforms the tree types of annotatedParams to the Tree's where they're converted
-    // to Lists
-    val valsAsLists: List[Tree] = annotatedParams map { sourceParam =>
-      val paramValue: Tree = paramVal(obj, sourceParam)
-
-      //      sourceParam.typeSignature
-
-      sourceParam.typeSignature match {
-        case paramType if paramType <:< listOfTType =>
-          //echo(s"$sourceParam: List[T]")
-          paramValue
-        case paramType if paramType <:< iterableOfTType =>
-          //echo(s"$sourceParam: Iterable[T]")
-          Select(paramValue, newTermName("toList"))
-        case paramType if paramType <:< optionOfTType =>
-          //echo(s"$sourceParam: Option[T]")
-          Select(paramValue, newTermName("toList"))
-        case paramType if paramType <:< tType =>
-          //echo(s"$sourceParam: T")
-          Apply(
-            Select(Ident(listCompanionSymbol), newTermName("apply")),
-            List(paramValue)
-          )
-        case _ =>
-          abort(s"parameter ${sourceParam.name} of type $sourceTypeName with type ${sourceParam.typeSignature} is not compatible with ${tType}")
-      }
+    def echo(msg: String) {
+      c.echo(c.enclosingPosition, msg)
     }
 
-    val deepLists: List[Option[Tree]] = otherParams map { otherParam =>
-      val implicitExtractorType = getImplicitExtractorType(otherParam.typeSignature)
-      val extractorTree: Tree = c.inferImplicitValue(implicitExtractorType)
-      echo(s"otherParam: $otherParam, $implicitExtractorType ${showRaw(extractorTree)}")
-      if (extractorTree.isEmpty) {
-        None
-      } else {
-        val getValuesFunc = Select(extractorTree, newTermName("getValues"))
-        Some(Apply(getValuesFunc, List(paramVal(obj, otherParam))))
-      }
+    def abort(msg: String): Nothing = {
+      c.abort(c.enclosingPosition, msg)
     }
-    
+
+    val sourceTypeName = sourceType.typeSymbol.name
+
+    val sourceTypeNameExpr = c.Expr[String](Literal(Constant(sourceTypeName.toString)))
+
+    val tTypeName = tType.typeSymbol.name
+    val tTypeNameExpr = c.Expr[String](Literal(Constant(tTypeName.toString)))
+
+    val tCompanion = tType.typeSymbol.companionSymbol
+
+    def hasTAnn(param: Symbol): Boolean = {
+      param.annotations.exists(_.tpe =:= annType)
+    }
+    val sourceParams = caseClassParams(sourceType)
+
+    val annotatedParams: List[Symbol] = sourceParams.filter(hasTAnn)
+
+    def isCaseClass(param: Symbol): Boolean = {
+      param.typeSignature <:< c.weakTypeOf[Product]
+    }
+
+    def isIterableParamWithCaseClass(param: Symbol): Boolean = {
+      param.typeSignature <:< iterableType
+      //TODO check type argument isCaseClass
+    }
+
+    def isOptionParamWithCaseClass(param: Symbol): Boolean = {
+      param.typeSignature <:< optionType
+    }
+
+    val otherParams: List[Symbol] = {
+      sourceParams.filterNot(hasTAnn)
+    }
+
+    def getImplicitExtractorType(paramSourceType: c.Type): c.Type = {
+      appliedType(extractorType, List(paramSourceType, tType, annType))
+    }
+
+    //  val deepIterableParams = otherParams.filter { p =>
+    //    p.typeSignature <:< iterableType
+    //  }
+    //
+    //  val deepOptionParams = otherParams.filter { p =>
+    //    p.typeSignature <:< optionType
+    //  }
+    //
+    //  val deepCaseClassParams = otherParams.filter(isCaseClass)
 
 
-    Select(
-      Apply(
-        Select(Ident(listCompanionSymbol), newTermName("apply")),
-        valsAsLists ++ deepLists.flatten
-      ),
-      newTermName("flatten")
-    )
-  }
+    val listType = c.weakTypeOf[List[_]]
+    val listOfTType = appliedType(listType, List(tType))
+    val listCompanionSymbol = listType.typeSymbol.companionSymbol
 
-  /**
-   * Creates the body of a mapValue method.
-   */
-  def getValuesBody: c.Expr[List[T]] = {
+    val iterableType = c.weakTypeOf[Iterable[_]]
+    val iterableOfTType = appliedType(iterableType, List(tType))
+
+    val optionType = c.weakTypeOf[Option[_]]
+    val optionOfTType = appliedType(optionType, List(tType))
 
 
-    echo(s"$sourceType matchingParams: $annotatedParams")
-    echo(s"$sourceType deepParams: $otherParams")
+    //val reader = c.inferImplicitValue(appliedType(readerType, List(A)))
+    //if(! reader.isEmpty)
 
-    val obj = Ident(newTermName("obj"))
-    val tree = mkListResult(obj)
-    echo(s"TREE: ${showRaw(tree)}")
-    c.Expr[List[T]](tree)
-    //
-    //    // The list of trees will pass as arguments to the constructor
-    //    val values: List[Tree] = {
-    //      // a reference to the TFrom object
-    //
-    //      // for each of the parameters to TTo.apply, make a tree that Selects the values with the same name
-    //      // from the TFrom object
-    //
-    //      val listTrees = listParams.map {
-    //        case (annotationName, param) =>
-    //          val sources = extractables(annotationName)
-    //          AssignOrNamedArg(Ident(param.name), )
-    //      }
-    //      val optionTrees = optionParams.map {
-    //        case (annotationName, param) =>
-    //          AssignOrNamedArg(Ident(param.name),
-    //            param.typeSignature match {
-    //              case definitions.IntTpe =>
-    //                Literal(Constant(0))
-    //
-    //              case _ =>
-    //                Literal(Constant(null))
-    //            })
-    //
-    //      }
-    //      requiredTrees.toList ++ optionTrees ++ listTrees
-    //    }
-    //
-    //    showRaw(values)
-    //
-    //    // make a tree that would call TTo's companion's object apply function with the values
-    //    // e.g. TTo.apply(value, value, ...)
-    //    val tree: Tree = Apply(constructorTree, values)
-    //
-    //    // convert the tree to an expression
-    //    c.Expr(tree)
+    def paramVal(obj: Tree, param: Symbol): Tree =
+      Select(obj, param.name)
 
-    //abort("blaat")
-  }
+    def mkListResult(obj: Tree): Tree = {
 
-  /**
-   * Get the first parameter list from the constructor of a case class
-   */
-  private def caseClassParams(typ: c.Type): List[Symbol] = {
-    val cTor = typ.declaration(nme.CONSTRUCTOR).asMethod
-    if (cTor.paramss.size != 1)
-      c.abort(c.enclosingPosition, "ObjMapper only supports case classes with 1 parameter list")
-    cTor.paramss.head
+      // transforms the tree types of annotatedParams to the Tree's where they're converted
+      // to Lists
+      val valsAsLists: List[Tree] = annotatedParams map { sourceParam =>
+        val paramValue: Tree = paramVal(obj, sourceParam)
+
+        //      sourceParam.typeSignature
+
+        sourceParam.typeSignature match {
+          case paramType if paramType <:< listOfTType =>
+            //echo(s"$sourceParam: List[T]")
+            paramValue
+          case paramType if paramType <:< iterableOfTType =>
+            //echo(s"$sourceParam: Iterable[T]")
+            Select(paramValue, newTermName("toList"))
+          case paramType if paramType <:< optionOfTType =>
+            //echo(s"$sourceParam: Option[T]")
+            Select(paramValue, newTermName("toList"))
+          case paramType if paramType <:< tType =>
+            //echo(s"$sourceParam: T")
+            Apply(
+              Select(Ident(listCompanionSymbol), newTermName("apply")),
+              List(paramValue)
+            )
+          case _ =>
+            abort(s"parameter ${sourceParam.name} of type $sourceTypeName with type ${sourceParam.typeSignature} is not compatible with ${tType}")
+        }
+      }
+
+      val deepLists: List[Option[Tree]] = otherParams map { otherParam =>
+        val implicitExtractorType = getImplicitExtractorType(otherParam.typeSignature)
+        val extractorTree: Tree = c.inferImplicitValue(implicitExtractorType)
+        echo(s"otherParam: $otherParam, $implicitExtractorType ${showRaw(extractorTree)}")
+        if (extractorTree.isEmpty) {
+          //TODO: when do we need to raise an error?
+          None
+        } else {
+          val getValuesFunc = Select(extractorTree, newTermName("getValues"))
+          Some(Apply(getValuesFunc, List(paramVal(obj, otherParam))))
+        }
+      }
+
+
+
+      Select(
+        Apply(
+          Select(Ident(listCompanionSymbol), newTermName("apply")),
+          valsAsLists ++ deepLists.flatten
+        ),
+        newTermName("flatten")
+      )
+    }
+
+    /**
+     * Creates the body of a mapValue method.
+     */
+    def getValuesBody: c.Expr[List[T]] = {
+
+
+      echo(s"$sourceType matchingParams: $annotatedParams")
+      echo(s"$sourceType deepParams: $otherParams")
+
+      val obj = Ident(newTermName("obj"))
+      val tree = mkListResult(obj)
+      echo(s"TREE: ${showRaw(tree)}")
+      c.Expr[List[T]](tree)
+
+      //abort("blaat")
+    }
+
+    /**
+     * Get the first parameter list from the constructor of a case class
+     */
+    private def caseClassParams(typ: c.Type): List[Symbol] = {
+      val cTor = typ.declaration(nme.CONSTRUCTOR).asMethod
+      if (cTor.paramss.size != 1)
+        c.abort(c.enclosingPosition, "Extractor only supports case classes with 1 parameter list")
+      cTor.paramss.head
+    }
+
   }
 
 }
